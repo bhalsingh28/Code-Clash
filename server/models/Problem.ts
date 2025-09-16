d18850236e3d8f36d9ca943f7e07fc7d8001ec28
Bhal Singh <sibhal34@gmail.com>
2025-09-16 20:40:12 +0530

    Frontend

--- server/models/Problem.ts
+++ server/models/Problem.ts
@@ -1,13 +1,16 @@
 import mongoose, { Schema, Document } from "mongoose";
 
 export interface IProblem extends Document {
+  roomId: string; // reference to the room
   title: string;
   description: string;
   difficulty: "Easy" | "Medium" | "Hard";
+  participants: string[]; // users in that room
 }
 
 const ProblemSchema = new Schema<IProblem>(
   {
+    roomId: { type: String, required: true }, // can later be ObjectId if you have a Room model
     title: { type: String, required: true },
     description: { type: String, required: true },
     difficulty: {
@@ -15,6 +18,7 @@ const ProblemSchema = new Schema<IProblem>(
       enum: ["Easy", "Medium", "Hard"],
       required: true,
     },
+    participants: [{ type: String }],
   },
   { timestamps: true }
 );
